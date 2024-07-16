e752a9841bfe897c79c5be3d042b9c7eff40c728
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-07-16 13:38:47 +0000

    [Add] booksコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-    devise_for :users
     root to: "homes#top"
+    devise_for :users
+    
+    resources :books, only: [:index, :show, :edit]
+    
     get 'homes/about', as: 'about'
 end
