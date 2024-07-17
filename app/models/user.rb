7bff7412775888f57d08c862196c533e34460e53
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-07-17 10:31:16 +0000

    [Update]get_imegeメソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy
   
-  def get_image
+  def get_image(width = 100, height = 100)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_fill: [width, height]).processed
   end
   
 end
