23c8bbd6023478f38edfce15e086291a53c4f362
EC2 Default User <ec2-user@ip-172-31-0-160.ec2.internal>
2024-07-18 11:27:15 +0000

    [Add]bookにdestroy機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
@@ -31,12 +31,17 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
-  
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
