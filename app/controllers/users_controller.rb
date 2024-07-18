baecb19d8754db7ec7ab6023dfb5405a5e346bcb
EC2 Default User <ec2-user@ip-172-31-0-160.ec2.internal>
2024-07-18 09:16:01 +0000

    [Update]showページにテンプレート適用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,24 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    if user.update(user_params)
+      redirect_to user_path(user.id)
+    else
+      render :edit
+    end
   end
-  
-  
+
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :image)  end
