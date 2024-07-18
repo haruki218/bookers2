5aa4b9f6663af84e54f5fe56ae37f7716a389a00
EC2 Default User <ec2-user@ip-172-31-0-160.ec2.internal>
2024-07-18 10:17:41 +0000

    [Add]bookにupdate機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,7 +23,14 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
-  
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
+  end
   
   
   private
