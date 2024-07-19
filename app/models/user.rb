0e02303587fd36c1536ffcc26c4087d3c2853024
EC2 Default User <ec2-user@ip-172-31-0-160.ec2.internal>
2024-07-19 11:05:17 +0000

    [Update]aboutのurl、Backリンク修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
 class User < ApplicationRecord
-  
-  has_one_attached :image
+
+  has_one_attached :profile_image
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :books, dependent: :destroy
-  
+
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
-  
+
   def get_image(width = 100, height = 100)
-    unless image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_fill: [width, height]).processed
+    profile_image.variant(resize_to_fill: [width, height]).processed
   end
-  
+
 end
