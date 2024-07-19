12b2186963008a9fb61170830c92942eb25438ca
EC2 Default User <ec2-user@ip-172-31-0-160.ec2.internal>
2024-07-19 12:24:35 +0000

    [Finish]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -55,7 +55,8 @@ class BooksController < ApplicationController
   end
 
   def is_matching_login_user
-    user = User.find(params[:id])
+     @book = Book.find(params[:id])
+    user = @book.user
     unless user.id == current_user.id
       redirect_to books_path
     end
