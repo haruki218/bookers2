2d7bd53334c691cb82b29d30a000e15dac7d10e8
EC2 Default User <ec2-user@ip-172-31-0-160.ec2.internal>
2024-07-19 08:15:15 +0000

    [Add]フラッシュメッセージ、バリデーション、アクセス制限実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  
   def index
     @book = Book.new
     @books = Book.all
@@ -9,8 +10,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
+      @user = current_user
       render :index
     end
   end
@@ -26,10 +30,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
     else
+      @books = Book.all
+      @user = current_user
       render :edit
     end
   end
@@ -45,5 +52,6 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy
   
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+  
   def get_image(width = 100, height = 100)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
